8bdc98a8e8ee968814818c93af9a07ace4abb746
FRANK MUHINDO <mfrankzagarinoh@gmail.com>
2025-08-24 22:06:29 +0000

    what could be the problem FirebaseError: Firebase: Error (auth/configura

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAljCd6PAlBH88CwAKNE5im1tS89XbjrMg",
+  authDomain: "warehouse-sentinel.firebaseapp.com",
+  projectId: "warehouse-sentinel",
+  storageBucket: "warehouse-sentinel.firebasestorage.app",
+  messagingSenderId: "1036071190821",
+  appId: "1:1036071190821:web:33a7bd8ffa6179341fdacc",
 };
 
 // Initialize Firebase
